cad79ca476b11b5c044fe6d31e34c0a4c7cc0252
Onelio Viera <ojvwebdesign@gmail.com>
2023-10-20 07:47:40 -0400

    update files

--- CSX Unit 1/10-bracket-notation-strings.js
+++ CSX Unit 1/10-bracket-notation-strings.js
@@ -3,5 +3,17 @@
 // Name: Onelio Viera
 // Subject: Part 1 - Bracket Notation for Strings
 
+const songTitle = "Space Oddity"
+const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade"
 
+// Use bracket notation to console.log the following characters: 'S'.
+const firstLitter = songTitle[0];
+console.log(firstLitter);
 
+// Use bracket notation to console.log the following characters: 'o'.
+const sevenCharacter = songTitle[6];
+console.log(sevenCharacter);
+
+// Use bracket notation to console.log the following characters: 'e'.
+const lastLetter = lyrics[lyrics.length - 1];
+console.log(lastLetter);
---
+++ CSX Unit 1/11-numbers-add-sub.js
@@ -0,0 +1,10 @@
+// Codesmith: CSX
+// Date: 10.20.2023
+// Name: Onelio Viera
+// Subject: Part 1 - Numbers - Addition/Subtraction
+
+const birthYear = 1961;
+let age = 61;
+let in10Yrs = age + 10;
+
+console.log(in10Yrs);
