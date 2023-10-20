cfd9eec6add46f730acab793842274e89443b20a
Onelio Viera <ojvwebdesign@gmail.com>
2023-10-20 10:14:15 -0400

    update files

--- CSX Unit 1/12-numbers-mult-div.js
+++ CSX Unit 1/12-numbers-mult-div.js
@@ -3,3 +3,14 @@
 // Name: Onelio Viera
 // Subject: Part 1 - Numbers - Multiplication/Division
 
+let hometown = "New York City";
+let favAlbum = "The Rise and Fall of Ziggy Stardust and the Spiders From Mars";
+
+const count = (hometown.length);
+console.log(count);
+
+const upper = (favAlbum.toUpperCase());
+console.log(upper);
+
+const lower = (favAlbum.toLowerCase());
+console.log(lower);
